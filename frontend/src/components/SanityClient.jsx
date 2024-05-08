81a6fa57dc6e2343abfe33770679cfb86033d15c
fitorevs <142629110+fitorevs@users.noreply.github.com>
2024-05-08 22:42:25 +0200

    justering i sanityclient.jsx

--- frontend/src/components/SanityClient.jsx
+++ frontend/src/components/SanityClient.jsx
@@ -1,16 +1,16 @@
 import { createClient } from '@sanity/client'
 
+export const readClient = createClient({
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: 'production',
+  useCdn: true,
+  apiVersion: '2022-03-07',
+})
+
 export const writeClient = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
   dataset: 'production',
   useCdn: false,
-  apiVersion: '2024-06-05',
+  apiVersion: '2022-03-07',
   token: process.env.SANITY_USER_TOKEN
-})
-
-export const readClient = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: 'production',
-  apiVersion: '2024-06-05',
-  useCdn: true
 })
